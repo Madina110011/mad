854c3d5b897b3c83ea24dcfd05283710f0dbf0a7
Madina110011 <150903370+Madina110011@users.noreply.github.com>
2024-01-10 21:56:51 +0600

    Add files via upload

---
+++ Lesson-1.py
@@ -0,0 +1,24 @@
+'''a = 5
+b = 10
+
+print("Cумма:", a+b)
+print("Разность:",a-b)
+print("Деление:", a/b)
+print("Умножение:", a*b)'''
+
+'''name = "Мадина"
+city = "Astana"
+print (name +" "+ city) '''
+
+
+'''r= 10
+pi=3.14
+c=2*pi
+S=pi*r*r
+print("Длина окружности=", c , "Площадь окружности=",S)'''
+
+
+is_sunny=True
+temperature = 27
+print("На улице тепло?", is_sunny, "Температура?", temperature)
+
---
+++ Lesson-2.py
@@ -0,0 +1,18 @@
+'''name= "Madina"
+print(len(name))
+
+word= "Hello world"
+print (word.split(' '))
+
+text = "What a nice day!"
+print (text.replace('nice', 'good')) 
+#1
+e_mail = "madina_090698@mail.ru"
+position= e_mail.index('@')
+print (e_mail[:position])'''
+
+#2 
+value = "3*2"
+value2 = "7+15"
+print (eval(value))
+print (eval(value2))
